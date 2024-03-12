e00b8a5855d6ce555a6d8651144871eddf491c6d
Stephanie <Stephaniechloe21@gmail.com>
2024-03-13 00:19:52 +0100

    all good?

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,6 @@
 export default function updateUniqueItems(map) {
-  if (map instanceof Map)
-    return map.forEach((value, key) =>
-      map.set(key, +value === 1 ? 100 : value)
-    );
-  else throw new Error("Cannot process");
+  if (map instanceof Map) {
+    return map.forEach((value, key) => map.set(key, +value === 1 ? 100 : value));
+  }
+  throw new Error('Cannot process');
 }
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
 export default function cleanSet(set, startString) {
-  return set &&
-    startString &&
-    set instanceof Set &&
-    typeof startString === "string"
+  return set
+    && startString
+    && set instanceof Set
+    && typeof startString === 'string'
     ? Array.from(set)
-        .filter((string) => string.startsWith(startString))
-        .map((string) => string.slice(startString.length))
-        .join("-")
-    : "";
+      .filter((string) => string.startsWith(startString))
+      .map((string) => string.slice(startString.length))
+      .join('-')
+    : '';
 }
