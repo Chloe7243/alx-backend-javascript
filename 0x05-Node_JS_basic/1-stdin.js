b55000e27ebefc1af3d4b0c109f68a9dfeba77b4
Stephanie <Stephaniechloe21@gmail.com>
2024-05-01 11:22:45 +0100

    task 1 again

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,9 @@
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  console.log(`Your name is: ${name}`);
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+
+process.stdin.on("data", (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
-process.stdin.on('end', () => console.log('This important software is now closing'));
+process.stdin.on("end", () => {
+  process.stdout.write("This important software is now closing\n");
+});
