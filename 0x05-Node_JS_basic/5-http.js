a20c93cb41a6065dbb7412f6e3642d04939d2395
Stephanie <Stephaniechloe21@gmail.com>
2024-05-01 12:26:01 +0100

    lint 6

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,24 +1,24 @@
-const fs = require("fs");
-const http = require("http");
+const fs = require('fs');
+const http = require('http');
 
 const app = http.createServer((req, res) => {
-  if (req.url === "/students") {
-    const responseParts = ["This is the list of our students"];
+  if (req.url === '/students') {
+    const responseParts = ['This is the list of our students'];
     const path = process.argv[2];
     (() =>
       new Promise((resolve, reject) => {
-        fs.readFile(path, "utf-8", (err, data) => {
+        fs.readFile(path, 'utf-8', (err, data) => {
           if (err) {
-            responseParts.push("Cannot load the database");
+            responseParts.push('Cannot load the database');
           } else {
             const students = data
-              .split("\n")
+              .split('\n')
               .slice(1)
-              .map((student) => student.replace("\r", ""))
-              .filter((student) => student !== "");
+              .map((student) => student.replace('\r', ''))
+              .filter((student) => student !== '');
 
             const groupedStudents = students.reduce((obj, student) => {
-              const studentDetails = student.split(",");
+              const studentDetails = student.split(',');
               const groupName = studentDetails[3];
               if (!groupName) return obj;
               if (!obj[groupName]) obj[groupName] = [];
@@ -30,21 +30,21 @@ const app = http.createServer((req, res) => {
             Object.entries(groupedStudents).forEach(([key, value]) => {
               responseParts.push(
                 `Number of students in ${key}: ${value.length}. List: ${value
-                  .join(", ")
-                  .replace("/n", "")}`
+                  .join(', ')
+                  .replace('/n', '')}`
               );
             });
 
             resolve(true);
           }
-          const responseText = responseParts.join("\n");
+          const responseText = responseParts.join('\n');
           res.statusCode = 200;
-          res.setHeader("Content-Type", "text/plain");
-          res.setHeader("Content-Length", responseText.length);
+          res.setHeader('Content-Type', 'text/plain');
+          res.setHeader('Content-Length', responseText.length);
           res.write(Buffer.from(responseText));
         });
       }))();
-  } else res.end("Hello Holberton School!");
+  } else res.end('Hello Holberton School!');
 });
 
 app.listen(1245);
