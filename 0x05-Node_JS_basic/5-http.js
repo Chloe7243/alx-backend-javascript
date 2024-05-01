81b513fbabf1eb61f072470abd56c22a7d1739cb
Stephanie <Stephaniechloe21@gmail.com>
2024-05-01 11:57:36 +0100

    task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,23 +1,24 @@
-const fs = require('fs');
-const http = require('http');
+const fs = require("fs");
+const http = require("http");
 
 const app = http.createServer((req, res) => {
-  if (req.url === '/students') {
+  if (req.url === "/students") {
     return new Promise((resolve, reject) => {
       const path = process.argv[2];
-      res.write('This is the list of our students\n');
-      fs.readFile(path, 'utf-8', (err, data) => {
+      res.write("This is the list of our students\n");
+      fs.readFile(path, "utf-8", (err, data) => {
         if (err) {
-          reject(new Error('Cannot load the database'));
+          reject(new Error("Cannot load the database"));
         } else {
+          res.statusCode = 200;
           const students = data
-            .split('\n')
+            .split("\n")
             .slice(1)
-            .map((student) => student.replace('\r', ''))
-            .filter((student) => student !== '');
+            .map((student) => student.replace("\r", ""))
+            .filter((student) => student !== "");
 
           const groupedStudents = students.reduce((obj, student) => {
-            const studentDetails = student.split(',');
+            const studentDetails = student.split(",");
             const groupName = studentDetails[3];
             if (!groupName) return obj;
             if (!obj[groupName]) obj[groupName] = [];
@@ -29,15 +30,15 @@ const app = http.createServer((req, res) => {
           Object.entries(groupedStudents).forEach(([key, value]) => {
             console.log(
               `Number of students in ${key}: ${value.length}. List: ${value
-                .join(', ')
-                .replace('/n', '')}`
+                .join(", ")
+                .replace("/n", "")}`
             );
           });
           resolve(true);
         }
       });
     });
-  } else res.end('Hello Holberton School!');
+  } else res.end("Hello Holberton School!");
 });
 
 app.listen(1245);
