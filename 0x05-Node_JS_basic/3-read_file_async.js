8626db33b82eaa23c19c44595a48d4331dd70e49
Stephanie <Stephaniechloe21@gmail.com>
2024-05-01 11:40:19 +0100

    promise

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,34 +1,37 @@
-const fsPromises = require('fs/promises');
+const fs = require("fs");
+// const fsPromises = require("fs/promises");
 
-const countStudents = async (path) => {
-  try {
-    const data = await fsPromises.readFile(path, { encoding: 'utf8' });
-    const students = data
-      .split('\n')
-      .slice(1)
-      .map((student) => student.replace('\r', ''))
-      .filter((student) => student !== '');
+const countStudents = (path) =>
+  new Promise((resolve, reject) => {
+    fs.readFile(path, "utf-8", (err, data) => {
+      if (err) {
+        reject(new Error("Cannot load the database"));
+      }
+      const students = data
+        .split("\n")
+        .slice(1)
+        .map((student) => student.replace("\r", ""))
+        .filter((student) => student !== "");
 
-    const groupedStudents = students.reduce((obj, student) => {
-      const studentDetails = student.split(',');
-      const groupName = studentDetails[3];
-      if (!groupName) return obj;
-      if (!obj[groupName]) obj[groupName] = [];
-      obj[groupName].push(studentDetails[0]);
-      return obj;
-    }, {});
+      const groupedStudents = students.reduce((obj, student) => {
+        const studentDetails = student.split(",");
+        const groupName = studentDetails[3];
+        if (!groupName) return obj;
+        if (!obj[groupName]) obj[groupName] = [];
+        obj[groupName].push(studentDetails[0]);
+        return obj;
+      }, {});
 
-    console.log(`Number of students: ${students.length}`);
-    Object.entries(groupedStudents).forEach(([key, value]) => {
-      console.log(
-        `Number of students in ${key}: ${value.length}. List: ${value
-          .join(', ')
-          .replace('/n', '')}`
-      );
+      console.log(`Number of students: ${students.length}`);
+      Object.entries(groupedStudents).forEach(([key, value]) => {
+        console.log(
+          `Number of students in ${key}: ${value.length}. List: ${value
+            .join(", ")
+            .replace("/n", "")}`
+        );
+      });
     });
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
-};
+    resolve(true);
+  });
 
 module.exports = countStudents;
