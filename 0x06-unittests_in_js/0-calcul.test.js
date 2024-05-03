65fcf6e5599cbd3dbd2eb6218199a57e832902b2
Stephanie <Stephaniechloe21@gmail.com>
2024-05-03 10:40:37 +0100

    idk

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,13 +6,13 @@ describe('calculateNumber', () => {
   it('should return 4', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
-  it('should return 4', () => {
-    assert.strictEqual(calculateNumber(1.2, 3), 4);
-  });
   it('should return 5', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
   it('should return 5', () => {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+  it('should return 6', () => {
+    assert.strictEqual(calculateNumber(2.5, 3), 6);
   });
 });
