3275236a71981d4028dc8ea86ae641e7bc3f17ae
Stephanie <Stephaniechloe21@gmail.com>
2024-05-04 09:43:32 +0100

    eslint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,31 +1,32 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const countStudents = (path) => {
   try {
-    const data = fs.readFileSync(path, 'utf8');
+    const data = fs.readFileSync(path, "utf8");
     const students = data
-      .split('\n')
+      .split("\n")
       .slice(1)
-      .map((student) => student.replace('\r', ''))
-      .filter((student) => student !== '');
+      .map((student) => student.replace("\r", ""))
+      .filter((student) => student !== "");
     const groupedStudents = students.reduce((obj, student) => {
-      const studentDetails = student.split(',');
+      const studentDetails = student.split(",");
+      const data = obj;
       const groupName = studentDetails[3];
-      if (!obj[groupName]) obj[groupName] = [];
-      obj[groupName].push(studentDetails[0]);
-      return obj;
+      if (!data[groupName]) data[groupName] = [];
+      data[groupName].push(studentDetails[0]);
+      return data;
     }, {});
 
     console.log(`Number of students: ${students.length}`);
     Object.entries(groupedStudents).forEach(([key, value]) => {
       console.log(
         `Number of students in ${key}: ${value.length}. List: ${value
-          .join(', ')
-          .replace('/n', '')}`
+          .join(", ")
+          .replace("/n", "")}`
       );
     });
   } catch (err) {
-    throw new Error('Cannot load the database');
+    throw new Error("Cannot load the database");
   }
 };
 
